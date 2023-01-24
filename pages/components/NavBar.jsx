87ed1bfe67eed84fe3646d0e2cf5df7fdf953988
Pedro <pedrovenegasjob@gmail.com>
2023-01-23 20:10:57 -0400

    Portafolio 1.8

--- pages/components/NavBar.jsx
+++ pages/components/NavBar.jsx
@@ -1,35 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import Image from "next/image";
 import logo from "../../public/logoBlue.png";
 import { AiOutlineArrowUp } from "react-icons/ai";
 
 export const NavBar = ({ setDarkMode, darkMode }) => {
+  let Links = [
+    { name: "HOME", link: "/" },
+    { name: "SERVICE", link: "/" },
+    { name: "ABOUT", link: "/" },
+    { name: "BLOG'S", link: "/" },
+    { name: "CONTACT", link: "/" },
+  ];
+  let [open, setOpen] = useState(false);
+
   return (
     <nav
       id='nav'
       className='fixed w-full bg-white py-3 px-5 mb-3 flex justify-between dark:text-white dark:bg-black'
     >
-      <Image
-        src={logo}
-        alt='logo'
-        title='Pedro Venegas'
-        className='w-12 h-12 object-cover md:ml-10'
-      />
-      <p className='text-black'>PEDRO VENEGAS</p>
-      <ul className='flex items-center md:mr-5'>
-        <li>
+      <div className='flex'>
+        <Image
+          src={logo}
+          alt='logo'
+          title='Pedro Venegas'
+          className='w-12 h-12 object-cover md:ml-10'
+        />
+        <p className='text-black'>PEDRO VENEGAS</p>
+      </div>
+
+      <div className='flex items-center md:mr-5'>
+        <div>
           <BsFillMoonStarsFill
             onClick={() => setDarkMode(!darkMode)}
             className='cursor-pointer text-2xl'
           />
-        </li>
-        <li className='ml-8 cursor-pointer text-2xl'>
+        </div>
+
+        <div>
           <a href='#' className='transition-all ease-in duration-300'>
             <AiOutlineArrowUp />
           </a>
-        </li>
-      </ul>
+        </div>
+      </div>
     </nav>
   );
 };
